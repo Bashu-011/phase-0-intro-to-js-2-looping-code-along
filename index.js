59638cb19af16a648659b5908ca445ae323e46cf
Bashu-011 <bashu2609@gmail.com>
2023-12-17 13:45:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(array2, event){
+    let array1 = [];
+    for (let i = 0; i < array2.length; i++){
+        let string = `Thank you, ${array2[i]}, for the wonderful ${event} gift!`;
+        array1.push(string);
+        }
+    return (array1);
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(x){
+    while (x >= 0){
+        console.log(x);
+        x--
+    }
+}
+
